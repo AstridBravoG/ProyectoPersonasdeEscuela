e11059d3659fd6ca011e0e9533062d6ceef8cdc5
AstridBravoG <lb725054@gmail.com>
2023-10-12 12:18:13 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoPersonasdeEscuela/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace ProyectoPersonasdeEscuela
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbTipoPersona = new System.Windows.Forms.GroupBox();
+            this.radEstudiante = new System.Windows.Forms.RadioButton();
+            this.radProfesor = new System.Windows.Forms.RadioButton();
+            this.btnCapturarDatos = new System.Windows.Forms.Button();
+            this.btnMostrarDatos = new System.Windows.Forms.Button();
+            this.grbDatos = new System.Windows.Forms.GroupBox();
+            this.txtCalificacion = new System.Windows.Forms.TextBox();
+            this.txtSueldo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.datagvMostrar = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sueldo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Calificacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.grbTipoPersona.SuspendLayout();
+            this.grbDatos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagvMostrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grbTipoPersona
+            // 
+            this.grbTipoPersona.Controls.Add(this.radEstudiante);
+            this.grbTipoPersona.Controls.Add(this.radProfesor);
+            this.grbTipoPersona.Location = new System.Drawing.Point(45, 46);
+            this.grbTipoPersona.Name = "grbTipoPersona";
+            this.grbTipoPersona.Size = new System.Drawing.Size(137, 104);
+            this.grbTipoPersona.TabIndex = 0;
+            this.grbTipoPersona.TabStop = false;
+            this.grbTipoPersona.Text = "Tipo de Persona";
+            // 
+            // radEstudiante
+            // 
+            this.radEstudiante.AutoSize = true;
+            this.radEstudiante.Location = new System.Drawing.Point(33, 63);
+            this.radEstudiante.Name = "radEstudiante";
+            this.radEstudiante.Size = new System.Drawing.Size(75, 17);
+            this.radEstudiante.TabIndex = 1;
+            this.radEstudiante.TabStop = true;
+            this.radEstudiante.Text = "Estudiante";
+            this.radEstudiante.UseVisualStyleBackColor = true;
+            this.radEstudiante.CheckedChanged += new System.EventHandler(this.radEstudiante_CheckedChanged);
+            // 
+            // radProfesor
+            // 
+            this.radProfesor.AutoSize = true;
+            this.radProfesor.Location = new System.Drawing.Point(33, 30);
+            this.radProfesor.Name = "radProfesor";
+            this.radProfesor.Size = new System.Drawing.Size(64, 17);
+            this.radProfesor.TabIndex = 0;
+            this.radProfesor.TabStop = true;
+            this.radProfesor.Text = "Profesor";
+            this.radProfesor.UseVisualStyleBackColor = true;
+            this.radProfesor.CheckedChanged += new System.EventHandler(this.radProfesor_CheckedChanged);
+            // 
+            // btnCapturarDatos
+            // 
+            this.btnCapturarDatos.Location = new System.Drawing.Point(303, 41);
+            this.btnCapturarDatos.Name = "btnCapturarDatos";
+            this.btnCapturarDatos.Size = new System.Drawing.Size(110, 52);
+            this.btnCapturarDatos.TabIndex = 1;
+            this.btnCapturarDatos.Text = "Capturar datos";
+            this.btnCapturarDatos.UseVisualStyleBackColor = true;
+            this.btnCapturarDatos.Click += new System.EventHandler(this.btnCapturarDatos_Click);
+            // 
+            // btnMostrarDatos
+            // 
+            this.btnMostrarDatos.Location = new System.Drawing.Point(303, 109);
+            this.btnMostrarDatos.Name = "btnMostrarDatos";
+            this.btnMostrarDatos.Size = new System.Drawing.Size(110, 52);
+            this.btnMostrarDatos.TabIndex = 2;
+            this.btnMostrarDatos.Text = "Mostrar datos";
+            this.btnMostrarDatos.UseVisualStyleBackColor = true;
+            this.btnMostrarDatos.Click += new System.EventHandler(this.btnMostrarDatos_Click);
+            // 
+            // grbDatos
+            // 
+            this.grbDatos.Controls.Add(this.txtCalificacion);
+            this.grbDatos.Controls.Add(this.txtSueldo);
+            this.grbDatos.Controls.Add(this.txtNombre);
+            this.grbDatos.Controls.Add(this.label3);
+            this.grbDatos.Controls.Add(this.label2);
+            this.grbDatos.Controls.Add(this.label1);
+            this.grbDatos.Location = new System.Drawing.Point(45, 167);
+            this.grbDatos.Name = "grbDatos";
+            this.grbDatos.Size = new System.Drawing.Size(368, 132);
+            this.grbDatos.TabIndex = 3;
+            this.grbDatos.TabStop = false;
+            this.grbDatos.Text = "Datos de la Persona";
+            // 
+            // txtCalificacion
+            // 
+            this.txtCalificacion.Location = new System.Drawing.Point(100, 98);
+            this.txtCalificacion.Name = "txtCalificacion";
+            this.txtCalificacion.Size = new System.Drawing.Size(100, 20);
+            this.txtCalificacion.TabIndex = 5;
+            // 
+            // txtSueldo
+            // 
+            this.txtSueldo.Location = new System.Drawing.Point(100, 64);
+            this.txtSueldo.Name = "txtSueldo";
+            this.txtSueldo.Size = new System.Drawing.Size(142, 20);
+            this.txtSueldo.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(100, 31);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(202, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sueldo: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Calificación: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre: ";
+            // 
+            // datagvMostrar
+            // 
+            this.datagvMostrar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagvMostrar.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Sueldo,
+            this.Calificacion});
+            this.datagvMostrar.Location = new System.Drawing.Point(45, 323);
+            this.datagvMostrar.Name = "datagvMostrar";
+            this.datagvMostrar.Size = new System.Drawing.Size(368, 142);
+            this.datagvMostrar.TabIndex = 4;
+            this.datagvMostrar.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Sueldo
+            // 
+            this.Sueldo.HeaderText = "Sueldo";
+            this.Sueldo.Name = "Sueldo";
+            // 
+            // Calificacion
+            // 
+            this.Calificacion.HeaderText = "Calificación";
+            this.Calificacion.Name = "Calificacion";
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(435, 364);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(71, 52);
+            this.btnBorrar.TabIndex = 5;
+            this.btnBorrar.Text = "Borrar Datos";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(530, 477);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.datagvMostrar);
+            this.Controls.Add(this.grbDatos);
+            this.Controls.Add(this.btnMostrarDatos);
+            this.Controls.Add(this.btnCapturarDatos);
+            this.Controls.Add(this.grbTipoPersona);
+            this.Name = "Form1";
+            this.Text = "PERSONAS DE UNA ESCUELA";
+            this.grbTipoPersona.ResumeLayout(false);
+            this.grbTipoPersona.PerformLayout();
+            this.grbDatos.ResumeLayout(false);
+            this.grbDatos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagvMostrar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbTipoPersona;
+        private System.Windows.Forms.RadioButton radEstudiante;
+        private System.Windows.Forms.RadioButton radProfesor;
+        private System.Windows.Forms.Button btnCapturarDatos;
+        private System.Windows.Forms.Button btnMostrarDatos;
+        private System.Windows.Forms.GroupBox grbDatos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCalificacion;
+        private System.Windows.Forms.TextBox txtSueldo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView datagvMostrar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sueldo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Calificacion;
+    }
+}
+
